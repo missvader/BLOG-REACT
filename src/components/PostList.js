4616c866e38fc3f2f2ae951013a07f6466e913ef
missvader <vanepalazon611@gmail.com>
2022-12-14 15:13:23 +0100

    obtener listado posts con useContext en PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,11 @@
+import { useContext } from 'react';
+import { AppContext } from '../context/AppState';
 import PostItem from './PostItem';
 
 const PostList = () => {
   //Este componente es donde recorremos las publicaciones disponibles. Cada publicación activará un componente (el mismo componente) que leerá el título y el cuerpo de la publicación.
-  const posts = [{ id: 1, title: 'a title', body: 'a body' }];
-
+  //para obtener los posts que tenemos en AppContext usamos el hook useContext
+  const { posts } = useContext(AppContext);
   return (
     <ul>
       {posts.map((post) => (
