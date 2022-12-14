e16ded8d375987544178f22847cfda5676cbb741
missvader <vanepalazon611@gmail.com>
2022-12-14 15:02:01 +0100

    actualizando app.js con AppProvider y AppContext

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
 import "./App.dark.css";
-//Este blog tendrá un tema oscuro y uno claro, el tema oscuro aun no está configurado, por eso no funciona aun
+//Este blog tendrá un tema oscuro y uno claro
 import Header from './components/Header';
 import PostList from './components/PostList';
+import {AppContext, AppProvider} from "./context/AppState";
 
 function App() {
   return (
-    <div>
+    <AppProvider>
       <Header />
-      <main>
-        <h3>
-          New Posts: <span>1 posts</span> {/*hard-coded, will change later*/}
-        </h3>
-        <PostList />
-      </main>
-    </div>
+      <AppContext.Consumer>
+        {({ posts, darkTheme, setDarkTheme }) => (
+          <>
+            <main className={`${darkTheme ? 'dark' : ''}`}>
+              <h3>
+                New Posts: <span>1 posts</span> {/*hard-coded, will change later*/}
+              </h3>
+              <PostList />
+            </main>
+            <footer
+              onClick={() => setDarkTheme(!darkTheme)}
+              className={`${darkTheme ? 'dark' : ''}`}
+            >
+              <i className={`fas fa-${darkTheme ? 'sun' : 'moon'}`}></i>
+            </footer>
+          </>
+        )}
+        </AppContext.Consumer>
+    </AppProvider>
+    
   );
 }
 
