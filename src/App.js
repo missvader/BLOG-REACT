d599d2769bb2e32c0c6c85056d1fbfcdcc6de18b
missvader <vanepalazon611@gmail.com>
2022-12-14 12:27:44 +0100

    actualizando estilos. creado archivo para tema oscuro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import "./App.dark.css";
+//Este blog tendrá un tema oscuro y uno claro, el tema oscuro aun no está configurado, por eso no funciona aun
 import Header from './components/Header';
 import PostList from './components/PostList';
 
